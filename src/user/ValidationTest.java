2aa00370bb76522ce2690cc4c1009aba9403d316
vamsi-devops <60419553+vamsi-devops@users.noreply.github.com>
2022-08-23 13:31:36 +0530

    Update ValidationTest.java

--- src/user/ValidationTest.java
+++ src/user/ValidationTest.java
@@ -10,7 +10,7 @@ public class ValidationTest  {
 	@Test
     public void myTest1() {
 		validator v1=new validator();
-		boolean val=v1.Validate("vamsi", "Vamsi123");
+		boolean val=v1.Validate("Vamsi", "Vamsi123");
 		Assert.assertEquals(true, val);
 	}
 	
