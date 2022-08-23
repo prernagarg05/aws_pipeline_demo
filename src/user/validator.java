0cb83e01b0d8cb26c582c5a70aecedb25c1f88ed
vamsi-devops <60419553+vamsi-devops@users.noreply.github.com>
2022-08-23 13:31:20 +0530

    Update validator.java

--- src/user/validator.java
+++ src/user/validator.java
@@ -4,7 +4,7 @@ public class validator {
 
 	public static Boolean Validate(String UserName,String Password) {
 //Testing via Test Automa//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automation//Testing via Test Automationt//Testing via Test Automationion	// TODO Auto-generated method stub
-		if(UserName.equals("Surabhi") && Password.equals("Infy123"))
+		if(UserName.equals("Vamsi") && Password.equals("Vamsi123"))
 		{
 			return true;
 		}
